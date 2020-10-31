a40343bef71287c22dc2c73a9c157da560d87549
Nadia <nadiasierra2013@gmail.com>
2020-10-31 16:45:49 -0300

    cambios eda2

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ let paises = document.getElementsByName("exterior");
 let paisesVisitados = document.getElementById("paises-visitados");
 let direccion = document.getElementById("direccion");
 let ciudad = document.getElementById("ciudad");
-let formDetalles = document.getElementById('formulario-de-detalles')
+let formDetalles = document.getElementById('formulario-de-detalles');
 
 
 function displayPais(params) {
@@ -13,13 +13,46 @@ function displayPais(params) {
 }
 
 function displayDireccion(params) {
-    if (params == "si")  {
-        direccion.style.display='inline-block';
-        ciudad.style.display='inline-block';
+        if (params == "si")  {
+            direccion.style.display='inline-block';
+            ciudad.style.display='inline-block';
     }
         else {
             direccion.style.display='none';
-        ciudad.style.display='none';
+            ciudad.style.display='none';
         } 
 }
 
+var regexCampoNumerico=/^[A-Z]{4}+\-[0-9]{4}$/
+var regexEmail=/^[0-9a-zA-Z._.-]+\@[0-9a-zA-Z._.-]+\.[0-9a-zA-Z]+$/
+
+function validar(){
+    var error=false;
+    var mensajesError="";
+    if(document.getElementById("nombre-contacto").value==''){
+        error=true;
+        mensajesError+="<p>El campo nombre y apellido no puede estar vacío</p>";
+    }
+    if(!regexEmail.test(document.getElementById("email-contacto").value)){
+        error=true;
+        mensajesError+="<p>Debe escribir un mail válido</p>";
+    }
+    if (!regexCampoNumerico.test(document.getElementById("telefono-contacto").value)){
+        error=true;
+        mensajesError+="<p>Debe escribir un telefono válido</p>";
+    }
+    if(error){
+        document.getElementById("mensaje").innerHTML=mensajesError;
+        return false;
+    }else{
+        return true;
+    }
+}
+
+const CARACTERES=1000;
+
+function contarCaracteres() {
+    var caracteresEscritos=document.getElementById("consulta").value.length;
+    var restantes=CARACTERES-caracteresEscritos;
+    document.getElementById("caracteres").innerHTML=restantes;
+}
