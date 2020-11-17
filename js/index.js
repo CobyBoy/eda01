f2896aefdee52649410da73473a00930249349b7
CobyBoy <aboutacoby@gmail.com>
2020-11-16 21:49:43 -0300

    modificada propiedad de slide

--- js/index.js
+++ js/index.js
@@ -235,7 +235,7 @@ function contarCaracteres() {
 //Slider
 $(document).ready(function () {
     $('.slider-container').slick({
-        infinite: true,
+        slidesToShow: 1,
         slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 2000,
