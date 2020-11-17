72667882b4bb1964459766912352e833ec30c73a
CobyBoy <aboutacoby@gmail.com>
2020-11-17 20:24:24 -0300

    separacion de archivos js

---
+++ js/indexSlider.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $('.slider-container').slick({
+        infinite: true,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    });
+});
